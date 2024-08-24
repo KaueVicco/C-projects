6a67be3a4e27ef5444fdc00b7763956164e267da
Kaue Vicco <145366958+KaueVicco@users.noreply.github.com>
2024-08-24 13:48:21 -0400

    First C programming Homework

---
+++ FileName.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+	printf("My name is Kaue Vicco\n");
+	printf("My hobbies are playing basketball and watching soccer\n");
+	int x = 10;
+	int y = 3;
+	double z = (double) x / y;
+	printf("Z: %lf",z);
+	return(0);
+}
