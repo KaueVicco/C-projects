4f9f9fc58f58c5e025aff0f2ebf1fe481f93ab7e
Kaue Vicco <145366958+KaueVicco@users.noreply.github.com>
2024-09-07 15:14:06 -0400

    Number finder within a 50 element array

---
+++ Homework3.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+    int myBigArray[50] = { 6,39,37,70,47,53,84,42,6,95,56,19,90,7,70,99,70,
+    44,23,52,42,82,40,17,33,12,62,55,24,74,60,84,47,87,59,90,91,10,
+    62,91,27,63,62,55,79,56,91,38,98,60 };
+    int search, counter = 0;
+    printf("What value do you want to search for? ");
+    scanf("%i", &search);
+    for (int i = 0; i < 49; i++)
+    {
+        if (search == myBigArray[i])
+        {
+            printf("Found %d at index %d\n",search,i);
+            counter++;
+        }
+    }
+    if (counter == 0)
+    {
+        printf("Could not find %d in the array\n", search);
+    }
+    printf("The number %d was found %d times", search, counter);
+}
