3e722e6c985190e65b030915d82bf20d428b8a4c
Kaue Vicco <145366958+KaueVicco@users.noreply.github.com>
2024-09-08 21:31:39 -0400

    Number finder within a 50 element array.
    All numbers range from 0 to 99. If the user types a number greater than 99, the program prompts a invalidation message telling the user to another number.

--- NumberFinder50.c
+++ NumberFinder50.c
@@ -6,10 +6,15 @@ void main() {
     44,23,52,42,82,40,17,33,12,62,55,24,74,60,84,47,87,59,90,91,10,
     62,91,27,63,62,55,79,56,91,38,98,60 };
     int search, counter = 0;
-    printf("What value do you want to search for? ");
+    printf("What value do you want to search for (From 0 to 99)? ");
     scanf("%i", &search);
     for (int i = 0; i < 49; i++)
     {
+        if (search > 99)
+        {
+            printf("Invalid value. Type another: ");
+            scanf("%i", &search);
+        }
         if (search == myBigArray[i])
         {
             printf("Found %d at index %d\n",search,i);
